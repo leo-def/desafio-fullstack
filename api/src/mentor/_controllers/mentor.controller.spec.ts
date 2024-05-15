7e96e468a642d5705d035ee1bece3b6b4bda1c9a
Leonardo Oliveira <leodefoliveira@gmail.com>
2024-05-15 00:10:14 -0300

    Add create mentor endpoint

--- api/src/mentor/_controllers/mentor.controller.spec.ts
+++ api/src/mentor/_controllers/mentor.controller.spec.ts
@@ -1,7 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { plainToInstance } from 'class-transformer';
+import { CreateMentorDTO } from '../_dtos/create-mentor.dto';
+import { MentorDTO } from '../_dtos/mentor.dto';
 import { MentorService } from '../_services/mentor.service';
 import { MentorController } from './mentor.controller';
+
 describe('MentorController', () => {
   let mentorController: MentorController;
   let mentorService: MentorService;
@@ -12,6 +15,7 @@ describe('MentorController', () => {
       providers: [
         {
           provide: MentorService,
+            create: jest.fn(),
         },
       ],
     }).compile();
@@ -22,6 +26,39 @@ describe('MentorController', () => {
 
   it('should be defined', () => {
     expect(mentorController).toBeDefined();
+  });
+
+
+  describe('create', () => {
+    it('should create mentor', async () => {
+      const mockCreateMentorDTO: CreateMentorDTO = {
+        "name": "Jose Silva",
+        "cpf": "11222333444",
+        "email": "jose-silva@gmail.com"
+      };;
+      const mockMentorDTO: MentorDTO = {
+        "id": "clw5wh1730000corwl0zgj4g8",
+        "name": "Jose Silva",
+        "cpf": "11222333444",
+        "email": "jose-silva@gmail.com",
+        "createdAt": new Date("2024-05-14T04:35:54.063Z"),
+        "createdBy": null,
+        "updatedAt": new Date("2024-05-14T04:35:54.063Z"),
+        "updatedBy": null
+      };;
+      jest.spyOn(mentorService, 'create').mockResolvedValueOnce({
+        "id": "clw5wh1730000corwl0zgj4g8",
+        "name": "Jose Silva",
+        "cpf": "11222333444",
+        "email": "jose-silva@gmail.com",
+        "createdAt": new Date("2024-05-14T04:35:54.063Z"),
+        "createdBy": null,
+        "updatedAt": new Date("2024-05-14T04:35:54.063Z"),
+        "updatedBy": null
+      });
+      const result = await mentorController.create(mockCreateMentorDTO);
+      expect(result).toEqual(plainToInstance(MentorDTO, mockMentorDTO));
+    });
   });
 
 });
--- api/src/mentor/_controllers/mentor.controller.ts
+++ api/src/mentor/_controllers/mentor.controller.ts
@@ -1,6 +1,10 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiOkResponse, ApiInternalServerErrorResponse, ApiResponse, ApiBody, ApiCreatedResponse, ApiParam } from '@nestjs/swagger';
 import { plainToInstance } from 'class-transformer';
+import { CreateMentorDTO } from '../_dtos/create-mentor.dto';
+import { MentorDTO } from '../_dtos/mentor.dto';
+import { CreateMentor } from '../_types/create-mentor';
+import { MentorService } from '../_services/mentor.service';
 
 @ApiTags('Mentor API | Mentors')
 @Controller('mentor')
@@ -8,5 +12,29 @@ export class MentorController {
 
 
     constructor(private readonly service: MentorService) {}
+    @ApiOperation({ summary: 'Create Mentor.' })
+    @ApiCreatedResponse({
+      description: 'Mentor created successfully.',
+      type: MentorResponseDTO,
+    })
+    @ApiInternalServerErrorResponse({
+      status: 500,
+      description: 'Internal server error.',
+    })
+    @ApiResponse({ 
+      status: 400, 
+      description: 'Bad Request.' 
+    })
+    @ApiBody({
+      description: 'Create mentor param.',
+      type: CreateMentorDTO
+    })
+    @Post()
+    async create(@Body() data: CreateMentorDTO): Promise<MentorDTO | undefined>{
+      const request =  plainToInstance(CreateMentor, data)
+      const response = await this.service.create(request)
+      return plainToInstance(MentorDTO, response)
+    }
+  
   }
   
