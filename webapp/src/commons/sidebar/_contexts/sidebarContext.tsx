69d1e00fb2f8dcd91c69f1a8424655f9fe773054
Leonardo de Freitas Oliveira <leodefoliveira@gmail.com>
2024-05-15 15:33:33 -0300

    feat: frontend - add sidebar context

---
+++ webapp/src/commons/sidebar/_contexts/sidebarContext.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { createContext } from "react";
+import { SidebarState } from "../_types/sidebarState";
+import { SidebarAction } from "../_types/sidebarAction";
+import { AppReducerContext } from "../../shared/_types/appReducerContext";
+export const initialState = {
+  open: undefined
+} as SidebarState;
+
+export const SidebarContext = createContext({
+  state: initialState,
+} as AppReducerContext<SidebarState, SidebarAction>);
+
+
+
