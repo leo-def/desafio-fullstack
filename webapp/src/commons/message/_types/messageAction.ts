9d8d017aa4159529358c991c8ad780d27775210c
Leonardo de Freitas Oliveira <leodefoliveira@gmail.com>
2024-05-15 15:22:17 -0300

    feat: frontend - add mesage types

---
+++ webapp/src/commons/message/_types/messageAction.ts
@@ -0,0 +1,6 @@
+import { AppAction } from "../../shared/_types/appAction";
+import { MessageActionTypeEnum } from "../_enums/messageActionType.enum";
+import { MessageActionPayload } from "./messageActionPayload";
+
+export interface MessageAction
+  extends AppAction<MessageActionTypeEnum, MessageActionPayload> {}
