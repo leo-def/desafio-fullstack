437fac2cd43d42c595bbf32f9ae5e16eaae8721a
Leonardo Oliveira <leodefoliveira@gmail.com>
2024-05-15 00:36:07 -0300

    Import db conn module in mentor module

--- api/src/mentor/mentor.module.ts
+++ api/src/mentor/mentor.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MentorController } from './_controllers/mentor.controller';
 import { MentorService } from './_services/mentor.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { PaginationModule } from 'src/pagination/pagination.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PaginationModule],
   controllers: [MentorController],
   providers: [MentorService],
   exports: [MentorService]
