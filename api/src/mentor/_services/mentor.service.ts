5ba6c0acf10fce3d56ca56cfb464ad70c2f79fba
Leonardo Oliveira <leodefoliveira@gmail.com>
2024-05-15 00:06:49 -0300

    Add create method to mentor service

--- api/src/mentor/_services/mentor.service.ts
+++ api/src/mentor/_services/mentor.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { Mentor } from '../_types/mentor';
+import { CreateMentor } from '../_types/create-mentor';
 
 @Injectable()
 export class MentorService {
     
+  constructor(
+    private readonly prisma: PrismaService,
+  ) {}
+
+
+  async create(data: CreateMentor): Promise<Mentor> {
+    return await this.prisma.mentor.create({ data });
+  }
 }
--- api/src/mentor/_services/mentors.service.spec.ts
+++ api/src/mentor/_services/mentors.service.spec.ts
@@ -1,19 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MentorService } from './mentor.service';
+import { PrismaService } from '../../prisma/prisma.service';
+
 describe('MentorService', () => {
   let mentorService: MentorService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         MentorService,
+        PrismaService,
       ],
     }).compile();
 
     mentorService = module.get<MentorService>(MentorService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(mentorService).toBeDefined();
   });
 
+
+  describe('create', () => {
+    it('should call prisma.mentor.create with the provided data', async () => {
+      const data = 
+      {
+        "name": "Joao Silveira",
+        "cpf": "11223443551",
+        "email": "joao-silveira@email.com"
+      };
+      jest.spyOn(prismaService.mentor, 'create').mockResolvedValueOnce(
+        {
+          "id": "clw5whkgi0001corwxykj7j2z",
+          "name": "Joao Silveira",
+          "cpf": "11223443551",
+          "email": "joao-silveira@email.com",
+          "createdAt": new Date("2024-05-14T04:36:19.026Z"),
+          "createdBy": null,
+          "updatedAt": new Date("2024-05-14T04:36:19.026Z"),
+          "updatedBy": null
+        });
+      await mentorService.create(data);
+      expect(prismaService.mentor.create).toHaveBeenCalledWith({ data });
+    });
+  });
 });
