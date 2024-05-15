524175f5a1985e186ef0163346aac231eeb16a59
Leonardo Oliveira <leodefoliveira@gmail.com>
2024-05-14 23:22:36 -0300

    Setup backend swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.ORIGIN ? process.env.ORIGIN.split(';') : [ 'http://localhost:8080'],
+    methods: ['POST', 'PUT', 'DELETE', 'GET'],
+  });
+  app.setGlobalPrefix('/api');
+  const config = new DocumentBuilder()
+    .setTitle('Mentors')
+    .setDescription('Mentors API')
+    .setVersion('1.0')
+    .addTag('mentor')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
