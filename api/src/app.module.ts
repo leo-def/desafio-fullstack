3a8e802c3a7389366d7dbce1a60de9c37c239f67
Leonardo Oliveira <leodefoliveira@gmail.com>
2024-05-14 23:55:38 -0300

    Load mentor module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MentorModule } from './mentor/mentor.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PaginationModule } from './pagination/pagination.module';
 
 @Module({
   imports: [
     PrismaModule,
+    MentorModule,
     PaginationModule
   ],
   controllers: [],
--- api/src/mentor/mentor.module.ts
+++ api/src/mentor/mentor.module.ts
@@ -1,5 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MentorController } from './_controllers/mentor.controller';
+import { MentorService } from './_services/mentor.service';
 
 @Module({
+  controllers: [MentorController],
+  providers: [MentorService],
+  exports: [MentorService]
 })
 export class MentorModule {}
