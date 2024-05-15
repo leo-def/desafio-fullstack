7e8702ce3a1e8580362188ab055e6c30a2b454fe
Leonardo Oliveira <leodefoliveira@gmail.com>
2024-05-14 23:20:48 -0300

    Setup backend cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.ORIGIN ? process.env.ORIGIN.split(';') : [ 'http://localhost:8080'],
+    methods: ['POST', 'PUT', 'DELETE', 'GET'],
+  });
+  app.setGlobalPrefix('/api');
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
