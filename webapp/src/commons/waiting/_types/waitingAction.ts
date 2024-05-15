852d5e40304113486f772368328421aae74ea63d
Leonardo de Freitas Oliveira <leodefoliveira@gmail.com>
2024-05-15 11:55:47 -0300

    feat: add waiting types

---
+++ webapp/src/commons/waiting/_types/waitingAction.ts
@@ -0,0 +1,7 @@
+
+import { AppAction } from "../../shared/_types/appAction";
+import { WaitingActionTypeEnum } from "../_enums/waitingActionType.enum";
+import { WaitingActionPayload } from "./waitingActionPayload";
+
+export interface WaitingAction
+  extends AppAction<WaitingActionTypeEnum, WaitingActionPayload> {}
