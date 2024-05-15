5a9f65b36816b003674f22552900fb536553b471
Leonardo Oliveira <leodefoliveira@gmail.com>
2024-05-15 00:38:35 -0300

    Add fetch method to mentor service

--- api/src/mentor/_services/mentor.service.ts
+++ api/src/mentor/_services/mentor.service.ts
@@ -3,14 +3,23 @@ import { PrismaService } from '../../prisma/prisma.service';
 import { Mentor } from '../_types/mentor';
 import { CreateMentor } from '../_types/create-mentor';
 import { UpdateMentor } from '../_types/update-mentor';
+import { PaginationService } from '../../pagination/_services/pagination.service';
+import { MentorPaginationParams } from '../_types/pagination/mentor-pagination-params';
+import { MentorPagination } from '../_types/pagination/mentor-pagination';
 
 @Injectable()
 export class MentorService {
     
   constructor(
     private readonly prisma: PrismaService,
+    private readonly pagination: PaginationService
   ) {}
 
+  async fetch(
+    params: MentorPaginationParams,
+  ): Promise<MentorPagination> {
+    return await this.pagination.fetch(this.prisma, 'mentor', params);
+  }
 
   async create(data: CreateMentor): Promise<Mentor> {
     return await this.prisma.mentor.create({ data });
--- api/src/mentor/_services/mentors.service.spec.ts
+++ api/src/mentor/_services/mentors.service.spec.ts
@@ -1,27 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MentorService } from './mentor.service';
+import { PaginationService } from '../../pagination/_services/pagination.service';
 import { PrismaService } from '../../prisma/prisma.service';
 
 describe('MentorService', () => {
   let mentorService: MentorService;
   let prismaService: PrismaService;
+  let paginationService: PaginationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         MentorService,
         PrismaService,
+        PaginationService,
       ],
     }).compile();
 
     mentorService = module.get<MentorService>(MentorService);
     prismaService = module.get<PrismaService>(PrismaService);
+    paginationService = module.get<PaginationService>(PaginationService);
   });
 
   it('should be defined', () => {
     expect(mentorService).toBeDefined();
   });
 
+
+  describe('fetch', () => {
+    it('should call paginationService.fetch with the provided params', async () => {
+      const params = {"skip":0,"take":5};
+      jest.spyOn(paginationService, 'fetch').mockResolvedValueOnce({
+        "items": [
+          {
+            "id": "clw5whkgi0001corwxykj7j2z",
+            "name": "Joao Silveira",
+            "cpf": "11223443551",
+            "email": "joao-silveira@email.com",
+            "createdAt": new Date("2024-05-14T04:36:19.026Z"),
+            "createdBy": null,
+            "updatedAt": new Date("2024-05-14T04:36:19.026Z"),
+            "updatedBy": null
+          },
+          {
+            "id": "clw5wi6pn0002corwiwszee88",
+            "name": "Luis Otavio",
+            "cpf": "99111222333",
+            "email": "luis-otavio@customail.com",
+            "createdAt": new Date("2024-05-14T04:36:47.867Z"),
+            "createdBy": null,
+            "updatedAt": new Date("2024-05-14T04:36:47.867Z"),
+            "updatedBy": null
+          },
+        ],
+        "params": params,
+        "count": 2
+      });
+      await mentorService.fetch(params);
+      expect(paginationService.fetch).toHaveBeenCalledWith(prismaService, 'mentor', params);
+    });
+  });
 
   describe('create', () => {
     it('should call prisma.mentor.create with the provided data', async () => {
