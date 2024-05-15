3a72ceb7e686bf94577c2b742ba60b2c554a2d01
Leonardo Oliveira <leodefoliveira@gmail.com>
2024-05-14 23:42:05 -0300

    Add pagination service

--- api/src/pagination/pagination.module.ts
+++ api/src/pagination/pagination.module.ts
@@ -2,5 +2,7 @@ import { Module } from '@nestjs/common';
 import { PaginationService } from './_services/pagination.service';
 
 @Module({
+    providers: [PaginationService],
+    exports: [PaginationService]
 })
 export class PaginationModule {}
