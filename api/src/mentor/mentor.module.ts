c269bdb14601a503c20422723bb148da01fc7487
Leonardo Oliveira <leodefoliveira@gmail.com>
2024-05-15 00:02:32 -0300

    Add db depedency to mentor module

--- api/src/mentor/mentor.module.ts
+++ api/src/mentor/mentor.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MentorController } from './_controllers/mentor.controller';
 import { MentorService } from './_services/mentor.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [MentorController],
   providers: [MentorService],
   exports: [MentorService]
