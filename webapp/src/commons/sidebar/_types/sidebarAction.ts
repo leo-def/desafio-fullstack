85f7e90683e45134da2b5b47718e11f892002f13
Leonardo de Freitas Oliveira <leodefoliveira@gmail.com>
2024-05-15 15:32:43 -0300

    feat: frontend - add sidebar types

---
+++ webapp/src/commons/sidebar/_types/sidebarAction.ts
@@ -0,0 +1,7 @@
+
+import { AppAction } from "../../shared/_types/appAction";
+import { SidebarActionTypeEnum } from "../_enums/sidebarActionType.enum";
+import { SidebarActionPayload } from "./sidebarActionPayload";
+
+export interface SidebarAction
+  extends AppAction<SidebarActionTypeEnum, SidebarActionPayload> {}
