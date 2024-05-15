f499ce027404748f5e69d21c70120c0cc318079d
Leonardo de Freitas Oliveira <leodefoliveira@gmail.com>
2024-05-15 15:16:03 -0300

    feat: add theme types

---
+++ webapp/src/commons/theme/_types/themeAction.ts
@@ -0,0 +1,7 @@
+
+import { AppAction } from "../../shared/_types/appAction";
+import { ThemeActionTypeEnum } from "../_enums/themeActionType.enum";
+import { ThemeActionPayload } from "./themeActionPayload";
+
+export interface ThemeAction
+  extends AppAction<ThemeActionTypeEnum, ThemeActionPayload> {}
