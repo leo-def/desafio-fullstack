245d3f22ed0b340f2d217fdfd40b0f9c0082affb
Leonardo de Freitas Oliveira <leodefoliveira@gmail.com>
2024-05-15 19:38:24 -0300

    feat: frontend - add mentor dto

---
+++ webapp/src/mentor/_types/mentor.dto.ts
@@ -0,0 +1,11 @@
+
+export interface MentorDto {
+  id: string;
+  name: string;
+  cpf: string;
+  email: string;
+  createdAt?: Date;
+  createdBy?: string;
+  updatedAt?: Date;
+  updatedBy?: string;
+}
